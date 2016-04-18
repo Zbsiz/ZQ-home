38a4c033f9d783979b8342635bc4305f30cb070e
root <root@asuka>
2016-04-19 05:11:24 +0800

    test-1

---
+++ python/test-1.py
@@ -0,0 +1,4 @@
+word = 'friends'
+find_the_evil_in_your_friends = \
+        word[0]+word[2:4]+word[-3:-1]
+print(find_the_evil_in_your_friends)
--- python/test.py
+++ python/test.py
@@ -4,3 +4,28 @@ string = 'bang!'
 total = string * (len(word) - num)
 print(len(word))
 print(total)
+
+phone_number = '1386-666-0006'
+hiding_number = phone_number.replace(phone_number[:9], '*')
+print(hiding_number)
+
+
+search = '168'
+num_a = '1386-168-0006'
+#print(search + ' is at ' + str(num_a.find(search)) + ' to ' + str(num_a.fiind(search)))
+
+print('{} a word she can get what she {} for.'.format('With', 'came'))
+print('{preposition} a word she can get what she {verb} for'.format(preposition = 'With', verb='came'))
+print('{0} a word she can  get what she {1} for.'.format('With', 'came'))
+
+# input("nihao")
+
+
+def nihao():
+    return 'nihao'
+
+def fahrenheit_converter(C):
+    fahrenheit = C*9/5+32
+    return str(fahrenheit) + '%f'
+
+fahrenheit_converter(32)
