6be9b21953274ec5ab9020a4e2a6a37819ac26d7
root <root@asuka>
2016-04-23 15:36:58 +0800

    练习

--- C/main.c
+++ C/main.c
@@ -4,5 +4,12 @@
 
 int main(int argc, char* argv[])
 {
+  printf("nihao!\n");  
+
+  int a = 1;
+  int b;
+
+  b=-a-5;
   
+  printf("%d\n", b);
 }
--- C/test.c
+++ C/test.c
@@ -8,5 +8,19 @@ int main()
 
   printf("%f\n", a);
   printf("%f\n", b);
+
+
+  int sos = 1;
+  float ciel = 2;
+  ciel = sos = 4;
+
+  printf("%.2f\n", sos);
+
+
+  while (1) {
+   +2sos;
+   printf("%d\n", sos);
+  }
+
   return 0;
 }
