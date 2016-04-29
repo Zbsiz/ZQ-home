8656e15dd104d35b83d31f29d9b7a5ff895728cc
sysop <sysop@asuka>
2016-04-29 19:04:06 +0800

    add the dokuwiki

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admins:$1$qqvD86vW$veOIRYcnzmIpC0Hocx9Lg.:邹强:foolchrome@gmail.com:admin,user
