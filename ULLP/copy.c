f78d1df6b37e1a0207c220412407f7d838a54fcd
foolchrome <foolchrome@gmail.com>
2016-08-22 20:49:34 +0800

    copy test2

--- ULLP/copy.c
+++ ULLP/copy.c
@@ -9,6 +9,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -19,19 +21,23 @@
 
 void oops(char *, char *);
 
-int main(int ac, char *av[])
+int main(int ac, char **av)
 {
   int in_fd, out_fd, n_chars;
   char buf[BUFFERSIZE];
+  char *destfilename;
+  char *make_destfilename(char *, char *);
 
   if (ac != 3) {
     fprintf(stderr, "usage: %s source destination\n", * av);
     exit(1);
   }
 
+  destfilename = make_destfilename(av[1], av[2]);
+
   if ((in_fd = open(av[1], O_RDONLY)) == -1)
     oops("Cannot open", av[1]);
-  if ((out_fd = creat(av[2], COPYMODE)) == -1)
+  if ((out_fd = creat(destfilename, COPYMODE)) == -1)
     oops("Cannot creat", av[2]);
 
   while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)
@@ -49,4 +55,26 @@ void oops(char *s1, char *s2)
   fprintf(stderr, "Error: %s", s1);
   perror(s2);
   exit(1);
+}
+
+char *make_destfilename(char *src, char *dest)
+{
+  struct stat info;
+  char *srcfilename;
+  char *rv;
+
+  if ( (srcfilename = strrchr(src, '/')) != NULL )
+    srcfilename++;
+  else
+    srcfilename = src;
+
+  rv = (char *) malloc(strlen(srcfilename) + strlen(dest) + 2);
+
+  if ( stat(dest, &info) == -1 )
+    return dest;
+  else
+    if ( S_ISDIR(info.st_mode) )
+      sprintf(rv, "%s/%s", dest, srcfilename);
+
+  return rv;
 }
